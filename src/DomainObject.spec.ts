ff74c0ae985333d9ee199f2d9cdb4e8ae88f36ef
charliematters <charliematters@gmail.com>
2020-10-09 13:54:52 +0100

    Initial commit

---
+++ src/DomainObject.spec.ts
@@ -0,0 +1,15 @@
+import  DomainObject  from"./DomainObject"
+
+describe("DomainObject", () => {
+  it("creates a domain object", () => {
+    const object = new DomainObject();
+    expect(object.foo).toBe(null);
+    expect(object.bar).toBe(null);
+  });
+
+  it("creates a domain object with data", () => {
+    const object = new DomainObject({foo:'foo', bar:'bar'});
+    expect(object.foo).toBe('foo');
+    expect(object.bar).toBe('bar');
+  });
+});
---
+++ src/DomainObject.ts
@@ -0,0 +1,12 @@
+import { pick } from "lodash";
+import { makeObservable, observable } from "mobx";
+type DomainObjectFields = Pick<DomainObject, "foo" | "bar">;
+
+export default class DomainObject {
+  @observable foo: string | null = null;
+  @observable bar: string | null = null;
+  constructor(initialData: Partial<DomainObjectFields> = {}) {
+    Object.assign(this, pick(initialData, Object.keys(this)));
+    makeObservable(this);
+  }
+}
